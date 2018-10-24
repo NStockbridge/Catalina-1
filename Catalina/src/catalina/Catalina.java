f2098349307818c83a84e987ddcd4c7ceb6f1210
perezgus <perezgus@011-4303-S27MC.students.cisd.conroeisd.loc>
2018-10-24 08:28:17 -0500

    Added Scanner

---
+++ Catalina/src/catalina/Catalina.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package catalina;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author perezgus
+ */
+public class Catalina {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner  scan = new Scanner(new File("Input.txt"));
+        
+    }
+    
+}
